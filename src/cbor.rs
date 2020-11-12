4f71725cab9f91929f4058d688baf4d42264a0a7
Peter Engelbert <pmengelbert@gmail.com>
2020-11-12 10:49:07 -0600

    Fix cbor parsing bug
    Previously, when options were parsed, the corresponding values were not
    being consumed if they were not one of the recognized strings.
    Authenticators may provide additional options, in which case the value
    should be consumed and ignored. Otherwise, an error will be thrown
    because the decoder encounters a boolean instead of the next integer key
    of the map.
    
    This small patch fixes this bug.
    
    Signed-off-by: Peter Engelbert <pmengelbert@gmail.com>

--- src/cbor.rs
+++ src/cbor.rs
@@ -415,7 +415,7 @@ impl OptionsInfo {
                 "clientPin" => options.client_pin = Some(decoder.bool()?),
                 "up" => options.up = decoder.bool()?,
                 "uv" => options.uv = Some(decoder.bool()?),
-                _ => continue,
+                _ => { decoder.bool()?; },
             }
         }
         Ok(options)
